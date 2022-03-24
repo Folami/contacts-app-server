3644c9233ecfe10eeec4093c5cd1f101f0ee0035
BIDMUS Folami <folami.bidmus@gmail.com>
2022-03-24 14:13:04 +0100

    eslint

--- index.js
+++ index.js
@@ -190,3 +190,14 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const app = require('./app') // the actual Express application
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
