8f55e1e4a9915acdc082ee530ea9a0ed2ccf0078
BIDMUS Folami <folami.bidmus@gmail.com>
2022-05-08 14:53:15 +0100

    uibuildRefactor

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-
+/*
 let contacts = [
     {
         'name': 'Arto Hellas',
@@ -44,18 +44,13 @@ let contacts = [
         'id': 5
     }
 ]
+*/
 const Contact = require('./models/contact')
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
-app.use(express.static('build'))
-
-const today = new Date()
-const timestamp = today.getTime()
-const standardTime = new Date(parseInt(timestamp)).toUTCString()
-let info = `<p>Contacts list has info for ${contacts.length} contacts</p>
-                <p>${standardTime}</p>`
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello Contacts!</h1>')
@@ -74,31 +69,46 @@ app.get('/info', (request, response) => {
     var dateTime = date+' '+time
     console.log(dateTime)
     */
-    response.send(info)
-})
-
-app.get('/api/contacts/:id', (request, response) => {
-    Contact.findById(request.params.id).then(contact => {
-        response.json(contact)
+    Contact.find({}).then(contacts => {
+        const today = new Date()
+        const timestamp = today.getTime()
+        const standardTime = new Date(parseInt(timestamp)).toUTCString()
+        let info = `<p>Contacts list has info for ${contacts.length} contacts</p>
+                        <p>${standardTime}</p>`
+        response.send(info)
     })
 })
 
-app.delete('/api/contacts/:id', (request, response) => {
-    const id = Number(request.params.id)
-    contacts = contacts.filter(contact => contact.id !== id)
-    response.status(204).end()
+app.get('/api/contacts/:id', (request, response, next) => {
+    Contact.findById(request.params.id)
+        .then(contact => {
+            if (contact) {
+                response.json(contact)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/contacts', (request, response) => {
-    const body = request.body
+app.delete('/api/contacts/:id', (request, response) => {
+    Contact.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
+app.post('/api/contacts', (request, response, next) => {
+    const body = request.body
+    /*
     if (body.name === undefined) {
         return response.status(400).json({ error: 'name missing' })
     }
     if (body.number === undefined) {
         return response.status(400).json({ error: 'numbert missing' })
     }
-
+    */
     const contact = new Contact({
         name: body.name,
         number: body.number,
@@ -107,16 +117,42 @@ app.post('/api/contacts', (request, response) => {
     contact.save().then(savedContact => {
         response.json(savedContact)
     })
+        .catch(error => next(error))
+})
+
+app.put('/api/contacts/:id', (request, response, next) => {
+    const { name, number } = request.body
+    Contact.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedContact => {
+            response.json(updatedContact)
+        })
+        .catch(error => next(error))
 })
 
 morgan('tiny')
-/*
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
-*/
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }   else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    next(error)
+}
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
